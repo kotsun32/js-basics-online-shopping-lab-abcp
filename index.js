529e6a9d5f38c931e281fedfc1427e755bf35a78
Sunny Kotwal <sunnykotwal@yahoo.com>
2018-01-25 20:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random()*99)
+ cart.push(new Object({[item]:price}))
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0){
+   console.log(`Your shopping cart is empty.`)
+ }
+ else{
+       var myString = "In your cart, you have "
+ 
+         for(var i = 0, l = cart.length; i < l; i++){
+           for(var list in cart[i]){
+             myString+=`${list} at $${cart[i][list]}`
+               if(i!==cart.length-1){
+                   myString+=", "
+                 }
+                 else{
+                   myString+="."
 }
-
+}
+}
+}
+ console.log(myString)
+ }
 function total() {
   // write your code here
 }
